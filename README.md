35bbe5525ed6af5ac528eac4720fc9579e30961e
kemokemo <t2wonderland@gmail.com>
2021-10-03 18:15:47 +0900

    feat: add id setter

---
+++ README.md
@@ -0,0 +1,3 @@
+# id-setter
+
+A sample program that opens an html file and assigns a serial number ID only to the class to be manipulated.
--- main.go
+++ main.go
@@ -9,6 +9,18 @@ import (
 	"golang.org/x/net/html"
 )
 
+var (
+	box        = "myBox"
+	classes    = []string{"myCheck", "myNote"}
+	classesMap = make(map[string]struct{})
+)
+
+func init() {
+	for _, v := range classes {
+		classesMap[v] = struct{}{}
+	}
+}
+
 func main() {
 	os.Exit(run())
 }
@@ -26,19 +38,54 @@ func run() int {
 		return 1
 	}
 
-	walkNodes(doc)
+	walkNodes(doc, 0, 1)
+
+	err = html.Render(os.Stdout, doc)
+	if err != nil {
+		fmt.Println("failed to render html, ", err)
+		return 1
+	}
 
 	return 0
 }
 
-func walkNodes(n *html.Node) {
+func walkNodes(n *html.Node, boxCounter int, elemCounter int) (int, int) {
 	if n.Type == html.ElementNode {
+		var contains bool
+		var cName string
+
 		for _, a := range n.Attr {
-			fmt.Printf("Attr: namespace=%v, key=%v, val=%v\n", a.Namespace, a.Key, a.Val)
+			if a.Key != "class" {
+				continue
+			}
+
+			if a.Val == box {
+				boxCounter++
+				n.Attr = append(n.Attr, html.Attribute{Key: "id", Val: getBoxID(boxCounter)})
+				continue
+			}
+
+			_, ok := classesMap[a.Val]
+			if ok {
+				contains = true
+				cName = a.Val
+			}
+			fmt.Printf("Attr: key=%v, val=%v\n", a.Key, a.Val)
+		}
+
+		if contains {
+			n.Attr = append(n.Attr, html.Attribute{Key: "id", Val: fmt.Sprintf("%s-%s-%d", getBoxID(boxCounter), cName, elemCounter)})
+			elemCounter++
 		}
 	}
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		walkNodes(c)
+		boxCounter, elemCounter = walkNodes(c, boxCounter, elemCounter)
 	}
+
+	return boxCounter, elemCounter
+}
+
+func getBoxID(counter int) string {
+	return fmt.Sprintf("box%v", counter)
 }
